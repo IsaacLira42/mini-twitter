60fb7ee59bcd12cd6d6e40cab5c0a7731131393a
IsaacLira42 <isaaclira422@gmail.com>
2026-03-23 00:09:16 -0300

    feat: atualiza a URL base da API para usar variável de ambiente

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import useUserStore from "../store/useUserStore";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
